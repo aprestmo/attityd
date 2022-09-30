0f633b5711fae22a96ccdc8990563af1289240c5
Alexander Prestmo <aprestmo@gmail.com>
2022-09-30 23:26:15 +0200

    Upgrade to 1.4.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), image()]
+  integrations: [compress(), sitemap(), image()]
 });

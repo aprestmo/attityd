172479a9ee8b0c25c1df17c6ba95c62b8523ecc6
Alexander Prestmo <aprestmo@gmail.com>
2022-12-24 13:37:07 +0100

    Add astro-icon and Rome config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import rome from "astro-rome";
 import sitemap from "@astrojs/sitemap";
@@ -6,6 +6,11 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), sitemap(), image(), rome({path: ['./src', './dist']})],
-  site: 'https://attityd.no'
+  integrations: [
+    compress(),
+    sitemap(),
+    image(),
+    rome({ path: ["./src", "./dist"] }),
+  ],
+  site: "https://attityd.no",
 });

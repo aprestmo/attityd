9d8b5ab6af568c976947f523ce5124a137cf1d75
Alexander Prestmo <aprestmo@gmail.com>
2023-04-12 13:46:55 +0200

    Add lightningcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import rome from "astro-rome";
+import lightningcss from "vite-plugin-lightningcss";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [rome()],
+	integrations: [],
+  vite: {
+    plugins: [
+      lightningcss({
+        browserslist: "> 2% in NO",
+      })
+    ]
+  }
 });

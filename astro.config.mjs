31d8356f5d32b9ead73ed9e41b3979b21dad9dfd
Alexander Prestmo <aprestmo@gmail.com>
2023-05-03 12:07:12 +0200

    Update to Astro 2.3.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import rome from "astro-rome";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     compress(),
     sitemap(),
-    image(),
     rome({ path: ["./src", "./dist"] }),
   ],
   site: "https://attityd.no",

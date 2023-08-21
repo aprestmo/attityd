3faae17868df13ad00eed70906fa53be016703f1
Alexander Prestmo <aprestmo@gmail.com>
2023-08-21 10:22:20 +0200

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-import compress from "astro-compress";
-import rome from "astro-rome";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    compress(),
     sitemap(),
-    rome({ path: ["./src", "./dist"] }),
   ],
   site: "https://attityd.no",
 });
